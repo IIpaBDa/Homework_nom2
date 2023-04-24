c209a6278cb73ce12b40e98e1fe18cbd70b298f3
Kelt <kelt_dmitriy@gmail.com>
2023-04-25 01:05:58 +0300

    add Homework Task 15

---
+++ Task_num15/Program.cs
@@ -0,0 +1,22 @@
+﻿//Семинар 2.
+//Домашнее задание.
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+//Выполнил: Кельт Дмитрий Владимирович (kelt_1981@mail.ru)
+//Решение:
+
+System.Console.WriteLine("Введите номер дня недели");
+string number = Console.ReadLine();
+int weekNumber = int.Parse(number);
+
+if (weekNumber == 6 || weekNumber == 7)
+{
+    System.Console.WriteLine($"День недели № {weekNumber} - выходной");
+}
+else if (weekNumber < 1 || weekNumber > 7)
+{
+    System.Console.WriteLine("Странный день недели");
+}
+else
+{
+    System.Console.WriteLine($"День недели № {weekNumber} - будний день");
+}
